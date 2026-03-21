18c52b0cdda88a7691fc8f5bd0fa5aabbd10090c
Amit Dubli <127493312+dubliamit@users.noreply.github.com>
2026-03-21 12:52:15 +0530

    Schema design

--- part1-rdbms/part1-rdbms_schema_design.sql
+++ part1-rdbms/part1-rdbms_schema_design.sql
@@ -1,55 +1,59 @@
 -- 1. Customers Table
+DROP TABLE IF EXISTS Customers;
 CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY,
+    customer_id VARCHAR(10) PRIMARY KEY,
     customer_name VARCHAR(100) NOT NULL,
     customer_email VARCHAR(100) NOT NULL UNIQUE,
     customer_city VARCHAR(50) NOT NULL
 );
 
 -- 2. Products Table
+DROP TABLE IF EXISTS Products;
 CREATE TABLE Products (
-    product_id INT PRIMARY KEY,
+    product_id VARCHAR(10) PRIMARY KEY,
     product_name VARCHAR(100) NOT NULL,
     category VARCHAR(50) NOT NULL,
     unit_price DECIMAL(10,2) NOT NULL
 );
 
 -- 3. Sales Representatives Table
+DROP TABLE IF EXISTS Sales_Reps;
 CREATE TABLE Sales_Reps (
-    sales_rep_id INT PRIMARY KEY,
+    sales_rep_id VARCHAR(10) PRIMARY KEY,
     sales_rep_name VARCHAR(100) NOT NULL,
     sales_rep_email VARCHAR(100) NOT NULL UNIQUE,
     office_address VARCHAR(200) NOT NULL
 );
 
 -- 4. Orders Table
+DROP TABLE IF EXISTS Orders;
 CREATE TABLE Orders (
-    order_id INT PRIMARY KEY,
-    customer_id INT NOT NULL,
-    sales_rep_id INT NOT NULL,
+    order_id VARCHAR(10) PRIMARY KEY,
+    customer_id VARCHAR(10) NOT NULL,
+    sales_rep_id VARCHAR(10) NOT NULL,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
-        ON UPDATE CASCADE
-        ON DELETE RESTRICT,
-    FOREIGN KEY (sales_rep_id) REFERENCES Sales_Reps(sales_rep_id)
-        ON UPDATE CASCADE
-        ON DELETE RESTRICT
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),        
+    FOREIGN KEY (sales_rep_id) REFERENCES Sales_Reps(sales_rep_id)  
 );
 
 -- 5. Order_Items Table
+DROP TABLE IF EXISTS Order_Items;
 CREATE TABLE Order_Items (
-    order_item_id INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT NOT NULL,
-    product_id INT NOT NULL,
+    order_item_id VARCHAR(10) PRIMARY KEY,
+    order_id VARCHAR(10) NOT NULL,
+    product_id VARCHAR(10) NOT NULL,
     quantity INT NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (product_id) REFERENCES Products(product_id)
-        ON UPDATE CASCADE
-        ON DELETE RESTRICT
 );
 
+SELECT * FROM Customers;
+SELECT * FROM Products;
+SELECT * FROM Sales_Reps;
+SELECT * FROM Orders;
+SELECT * FROM Order_Items;
+
+-- 5. Inserting values into all tables
 INSERT INTO Customers (customer_id, customer_name, customer_email, customer_city) VALUES
 ('C001','Rohan Mehta', 'rohan@gmail.com', 'Mumbai'),
 ('C002','Priya Sharma', 'priya@gmail.com', 'Delhi'),
@@ -70,15 +74,15 @@ INSERT INTO Sales_Reps (sales_rep_id, sales_rep_name, sales_rep_email, office_ad
 ('SR03','Ravi Kumar', 'ravi@corp.com', 'South Zone, MG Road, Bangalore - 560001');
 
 INSERT INTO Orders (order_id, customer_id, sales_rep_id, order_date) VALUES
-('ORD1000', 'C002', 'SR03','21-05-2023'),
-('ORD1001', 'C004', 'SR03','22-02-2023'),
-('ORD1002', 'C002', 'SR02','17-01-2023'),
-('ORD1003', 'C002', 'SR01','16-09-2023'),
-('ORD1004', 'C001', 'SR01','29-11-2023');
+('ORD1000', 'C002', 'SR03','2023-05-21'),
+('ORD1001', 'C004', 'SR03','2023-02-22'),
+('ORD1002', 'C002', 'SR02','2023-01-17'),
+('ORD1003', 'C002', 'SR01','2023-09-16'),
+('ORD1004', 'C001', 'SR01','2023-11-29');
 
-INSERT INTO Order_Items (order_id, product_id, quantity) VALUES
-('ORD1000', 'P001', 2),
-('ORD1001', 'P002', 5),
-('ORD1002', 'P005', 1),
-('ORD1003', 'P002', 5),
-('ORD1004', 'P005', 5);
+INSERT INTO Order_Items (order_item_id,order_id, product_id, quantity) VALUES
+(1,'ORD1000', 'P001', 2),
+(2,'ORD1001', 'P002', 5),
+(3,'ORD1002', 'P005', 1),
+(4,'ORD1003', 'P002', 5),
+(5,'ORD1004', 'P005', 5);
